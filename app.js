edecd809a1c02929989ab9b63379aabae116ffda
JohnAKASquib <johnakasquib96@gmail.com>
2020-07-07 18:00:38 -0400

    basic setup of arguments done

--- app.js
+++ app.js
@@ -1,2 +1,33 @@
 const { program } = require("commander");
 program.version("0.0.1");
+
+program
+  .option("-i, --input <path>", "specified path of input file")
+  .option("-o, --output <name>", "name of output file to be created")
+  .option(
+    "-f, --framework <type>",
+    "framework specified(should default to React) "
+  );
+program.parse(process.argv);
+
+try {
+  if (!program.input) {
+    throw "Input file path must be specified!";
+  }
+  if (!program.output) {
+    throw "An output file name must be specified!";
+  }
+  if (program.input) {
+    console.log("Path to input file is ", program.input);
+  }
+  if (program.output) {
+    console.log("Name of output file is ", program.output);
+  }
+  if (!program.framework) {
+    console.log("Specified framework is React");
+  } else {
+    console.log("Specified framework is ", program.framework);
+  }
+} catch (e) {
+  console.log(e);
+}
