4d7b7d52653710d7cb067a9c23050579d2c0436e
JohnAKASquib <johnakasquib96@gmail.com>
2020-07-08 21:27:13 -0400

    shell script now takes arguments as normal

--- src/generator.js
+++ src/generator.js
@@ -11,29 +11,30 @@ const path = require("path");
  * @param {String} file The name of the output file
  */
 const generator = (componentName, content, file) => {
-    // Check if any parameters are not of type string
-    if (typeof componentName !== 'string' || typeof content !== 'string' || typeof file !== 'string') {
-        throw "Error: All arguments must be of type string.";
+  // Check if any parameters are not of type string
+  if (
+    typeof componentName !== "string" ||
+    typeof content !== "string" ||
+    typeof file !== "string"
+  ) {
+    throw "Error: All arguments must be of type string.";
+  }
+
+  // React template is located in ./templates
+  nunjucks.configure("templates");
+  const res = nunjucks.render("react.njk", { componentName, content });
+
+  // Write the output file to the specified directory
+  fs.writeFile(path.join(OUTPUT_PATH, file), res, (err) => {
+    if (err) {
+      return console.log(err);
     }
-
-    // React template is located in ./templates
-    nunjucks.configure("templates");
-    const res = nunjucks.render(
-        "react.njk",
-        { componentName, content }
-    );
-
-    // Write the output file to the specified directory
-    fs.writeFile(path.join(OUTPUT_PATH, file), res, (err) => {
-      if (err) {
-          return console.log(err)
-      };
-    });
-}
+  });
+};
 
 // Example usage:
-// generator("Button", '<button type="submit">Alert</button>', "output.njk");
+//generator("Button", '<button type="submit">Alert</button>', "output.njk");
 
 module.exports = {
-    generator
+  generator,
 };
