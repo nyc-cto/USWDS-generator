11bdeb9cf11c7f07cb65ea2e646143c6300b917b
Chris Ewald <chrisewald@gmail.com>
2020-07-14 16:51:38 -0400

    Update bin/cli.js

--- bin/cli.js
+++ bin/cli.js
@@ -3,7 +3,6 @@
 var genFile = require("../src/generator");
 const { program } = require("commander");
 const fs = require("fs");
-const process = require("process");
 program.version("0.1");
 
 program
