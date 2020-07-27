b890bf69b380fc1e33f1c779a9579a3a0337c72c
Mathew Davidov <41017026+MathewDavidov@users.noreply.github.com>
2020-07-27 14:03:40 -0400

    Create a function to find all files with a given format and directory

---
+++ bin/fileSearch.js
@@ -0,0 +1,48 @@
+// https://coderrocketfuel.com/article/recursively-list-all-the-files-in-a-directory-using-node-js
+
+const fs = require("fs");
+const path = require("path");
+
+const getAllFiles = function (dirPath, arrayOfFiles, fileExtension) {
+  files = fs.readdirSync(dirPath);
+
+  arrayOfFiles = arrayOfFiles || [];
+
+  files.forEach(function (file) {
+    // If the "file" is a directory, recurse through the sub-directory finding all the files within the sub-directory
+    if (fs.statSync(dirPath + "/" + file).isDirectory()) {
+      arrayOfFiles = getAllFiles(
+        dirPath + "/" + file,
+        arrayOfFiles,
+        fileExtension
+      );
+    } else {
+      // If it is a file, compare if the file extension matches the file format parameter. If so, push it to the array
+      if (file.substring(file.lastIndexOf(".") + 1) === fileExtension) {
+        arrayOfFiles.push(path.join(__dirname, dirPath, "/", file));
+      }
+    }
+  });
+
+  return arrayOfFiles;
+};
+
+// Example usage
+const result = getAllFiles(
+  path.join(
+    __dirname,
+    "..",
+    "node_modules",
+    "uswds",
+    "src",
+    "components",
+    "07-form"
+  ),
+  null,
+  "njk"
+);
+console.log(result);
+
+module.exports = {
+  getAllFiles,
+};
